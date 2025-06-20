2bebd88eb864d4c8d647e1bee54485c8b784da4e
Jeff Thomas <jeffthomas013@gmail.com>
2025-06-20 11:58:21 +0100

    2

--- Drivers/WebDriverFactory.cs
+++ Drivers/WebDriverFactory.cs
@@ -6,6 +6,20 @@ public class WebDriverFactory : IWebDriverFactory
     public IWebDriver Create()
     {
         var options = new ChromeOptions();
+
+        // Check if running in headless mode (default to false if not set)
+        var headless = Environment.GetEnvironmentVariable("HEADLESS")?.ToLower() == "true";
+
+        if (headless)
+        {
+            options.AddArguments(
+                "--headless=new", 
+                "--disable-gpu", 
+                "--no-sandbox", 
+                "--disable-dev-shm-usage"
+            );
+        }
+
         return new ChromeDriver(options);
     }
 }
